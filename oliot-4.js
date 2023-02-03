fdcf4dc6f1bd32005316e0933d87059e475859d0
atlol <114558368+atlol@users.noreply.github.com>
2023-02-03 13:52:22 +0200

    Valmiimpi

--- oliot-4.js
+++ oliot-4.js
@@ -1,7 +1,10 @@
-var paivamaara = new Date();
-paivaysIlmanUCT = paivamaara.getDate() +"."+ paivamaara.getMonth()   +"."+ paivamaara.getFullYear() +" "+ paivamaara.getHours() +"."+ paivamaara.getMinutes() + "." +paivamaara.getSeconds();
-var updating = false;
 
+
+var nappi1 = document.getElementById("btn1").classList;
+
+// Päivämäärä dd.mm.yyyy hh.mm.ss
+
+var updating = false;
 var tilitehty = false;
 
 class Pankki {
@@ -10,106 +13,129 @@ class Pankki {
         this.saldo = Number(saldo);
         this.historia = historia;
     }
-    update(){
+    update(){ // Itestäänpäivittyvä historia
         if(updating == false){
             updating = true
+            nappi1.add("on")
             this.naytaTiedot();
         } else {
             updating = false
+            nappi1.remove("on")
         }
     }
     talleta() {
-        if(tilitehty == true){
-            var ennenTallete = this.saldo;
-            var tallete = Number(document.getElementsByClassName("talletus")[0].value);
-            if(tallete == 0){
-                document.getElementById("talleteIlmoite").innerHTML = "Et voi tallettaa 0€"
-            } else{
-            this.saldo += tallete;
-            // document.getElementById("talleteIlmoite").innerHTML = "Talletit saldoosi " + tallete + "€"
-            this.historia.push(paivaysIlmanUCT + ": Talletit " + tallete + "€" + ". Saldo ennen talletusta: "+ ennenTallete +"€. Saldo talletuksen jälkeen: " + this.saldo +"€"+"<br>")
-            document.getElementById("formTallete").value = "";
-            if(updating == true){
-                this.naytaTiedot();
+        if(tilitehty == true){ // Jos tilinumero on annettu
+            var ennenTallete = this.saldo; // Ottaa ennen talletusta olevan saldon
+            var tallete = Number(document.getElementsByClassName("talletus")[0].value) // Ottaa Inputista arvot
+            if(isNaN(tallete) == true){ // Jos sisältää kirjaimia
+                document.getElementById("talleteIlmoite").innerHTML = "Virhe: Syötä vain numeroita"
+                document.getElementById("formTallete").value = "";
+
+            } else {
+                if(this.saldo + tallete <0){ // Katsoo meneekö miinukselle
+                    document.getElementById("talleteIlmoite").innerHTML = "Virhe: Et voi nostaa miinuksella"
+                }else{
+                    if(tallete == 0){ // Jos ei talleta mitään
+                        document.getElementById("talleteIlmoite").innerHTML = "Et voi tallettaa 0€"
+                    } else{
+                    this.saldo += tallete; // Saldoon lisätään tallete
+                    document.getElementById("talleteIlmoite").innerHTML = "Talletit saldoosi " + tallete + "€"
+                    this.historia.push(hankiAika() + ": Talletit " + tallete + "€" + ". Saldo ennen talletusta: "+ ennenTallete +"€. Saldo talletuksen jälkeen: " + this.saldo +"€"+"<br>")
+                    // Työntää historiaan päiväyksen, talletuksen ja ennen jälkeen saldon
+                    document.getElementById("formTallete").value = ""; // Tyhjentää input-kentän
+                        if(updating == true){
+                            this.naytaTiedot(); // Päivittää itse jos päällä
+                        }
+                    }
+                }
             }
-            }
-        } else {
+        } else { // Jos tilinumeroa ei ole annettu
             alert("Syötä tilinumero")
             document.getElementById("formTallete").value = "";
         }
     }
     nosta() {
-    if(tilitehty == true){
-            var nosto = Number(document.getElementsByClassName("nosto")[0].value)
-            if(this.saldo == 0){
-                document.getElementById("nostoIlmoite").innerHTML = "Virhe: Saldosi on tyhjä"
-            } else {
-                var ennenNosto = this.saldo;
-                if(this.saldo - nosto <= -1){
-                    document.getElementById("nostoIlmoite").innerHTML = "Virhe: Yritit nostaa enemmän rahaa kuin sinulla"
+    if(tilitehty == true){ // Jos tilinumero on annettu
+            var nosto = Number(document.getElementsByClassName("nosto")[0].value) // Ottaa inputista arvon
+            if(isNaN(nosto) == true){ // Jos sisältää kirjaimia
+                document.getElementById("nostoIlmoite").innerHTML = "Virhe: Syötä vain numeroita"
+                document.getElementById("formNosto").value = ""; // Tyhjentää input kentän
+            }else {
+                if(this.saldo + nosto <0){ // Katsoo meneekö miinukselle
+                    document.getElementById("nostoIlmoite").innerHTML = "Virhe: Et voi nostaa miinuksella"
                 } else {
-                    if(nosto == 0){
-                        document.getElementById("nostoIlmoite").innerHTML = "Et voi nostaa 0€";
+                    if(this.saldo == 0){ // Jos yrität nostaa ja saldo == 0, et voi
+                        document.getElementById("nostoIlmoite").innerHTML = "Virhe: Saldosi on tyhjä"
                     } else {
-                        this.saldo -= nosto;
-                        // document.getElementById("nostoIlmoite").innerHTML = "Nostit saldostasi " + nosto + "€"
-                        this.historia.push(paivaysIlmanUCT + ": Nostit " + nosto + "€" + ". Saldo ennen nostoa: "+ ennenNosto  + "€. Saldo noston jälkeen: " + this.saldo+"€" + "<br>")
-                        if(updating == true){
-                            this.naytaTiedot();
+                        var ennenNosto = this.saldo; // Ottaa ennen nostoa olevan saldoluvun
+                        if(this.saldo - nosto <= -1){ // Jos saldo-nosto on -1 tai alle, tulee virheilmoitus
+                            document.getElementById("nostoIlmoite").innerHTML = "Virhe: Yritit nostaa enemmän rahaa kuin sinulla"
+                        } else {
+                            if(nosto == 0){ // Jos yrität nostaa 0€ tulee virheilmoitus
+                                document.getElementById("nostoIlmoite").innerHTML = "Et voi nostaa 0€";
+                            } else {
+                                this.saldo -= nosto; // Vähentää saldosta nostomäärän
+                                document.getElementById("nostoIlmoite").innerHTML = "Nostit saldostasi " + nosto + "€"
+                                this.historia.push(hankiAika() + ": Nostit " + nosto + "€" + ". Saldo ennen nostoa: "+ ennenNosto  + "€. Saldo noston jälkeen: " + this.saldo+"€" + "<br>")
+                                // Työntää historiaan päiväyksen, noston ja ennen jälkeen saldon
+                                if(updating == true){
+                                    this.naytaTiedot(); // Päivittää automaattisesti jos päällä
+                                }
+                            }
                         }
                     }
                 }
+                
             }
-            document.getElementById("formNosto").value = "";
-        } else {
+            document.getElementById("formNosto").value = ""; // Tyhjentää input-kentän
+        } else { // Jos tilinumeroa ei ole annettu
             alert("Syötä tilinumero")
-            document.getElementById("formTallete").value = "";
+            document.getElementById("formNosto").value = ""; // Tyhjentää input kentän
         }
     }
-    naytaTiedot() {
-        if(tilitehty == true){
-            document.getElementById("tilitiedot").innerHTML = "Tilinumero: " + this.tilinumero + "<br>" + "Saldo: " + this.saldo +"€"+ "<br>" + "Tilihistoria: " + this.historia 
-            console.table(this.historia);
-        } else {
+    naytaTiedot(historia) {
+        if(tilitehty == true){ // Jos tili on tehty
+            var historianTeksti = "";
+            document.getElementById("tilitiedot").innerHTML = " ";
+            for (let i = 1; i < this.historia.length; i++) {
+                historianTeksti += "<li>- " + this.historia[i] + "</li>"
+                document.getElementById("tilitiedot").innerHTML ="<li>" + "Tilinumero: " + this.tilinumero + "</li>" +"<li>" + "Saldo: " + this.saldo +"€"+ "<br>"  + "</li>" + "<li>" + "Tilihistoria: " + historianTeksti + "</li>";
+            }
+
+
+
+            // Näyttää tilinumeron, saldon ja tilihistorian.
+        } else { // Ei näytä jos tilinumeroa ei ole annettu
             alert("Syötä tilinumerosi")
         }
         
     }
-    
-    // naytaHistoria() {
-        
-    // }
 }
 
 var pankkiTili = new Pankki(undefined, 0, []);
+// Tekee uuden Pankin.      undefined on pitämässä paikkaa
 
-function kysy () {
-    var tilinNumero = document.getElementById("tilinumero").value;
-    if(tilinNumero == " " || tilinNumero == ""){
+function kysy (historia) {
+    var tilinNumero = document.getElementById("tilinumero").value; // Ottaa arvon input-kentästä
+
+    if(tilinNumero.trim() ==""){ // Jos välilyönnit otetaan pois ja tulee tyhjää, tulee virheilmoitus
         document.getElementById("tiliIlmoite").innerHTML = "Syötä tilinumerosi";
         alert("Syötä tilinumerosi");
     }else {
         document.getElementById("tiliIlmoite").innerHTML = "Tilinumero lisätty";
-        // this.historia.push(paivaysIlmanUCT + " Kirjauduit tilillesi")
-        pankkiTili = new Pankki(tilinNumero, 0, ["<br>"]);
-        tilitehty = true;
+        pankkiTili = new Pankki(tilinNumero, 0, ["<br>"]);//Undefined muutetaan 0, ja alkuun <br>
+        pankkiTili.historia.push(hankiAika() + " Kirjauduit tilillesi <br>");
+        tilitehty = true; // tili on tehty
     }
     
-    if(tilitehty == true){
-        var nappi1 = document.getElementById("btn1").classList;
+    if(tilitehty == true){ // Kun tilitehty autom. päivitys nappi tulee näkyviin
         nappi1.add("show");
     }
 }
 
-
-
-
-
-
-
-// Array historian sisälle:
-
-// Päivämäärä (Date-olio)
-// Tapahtuma (string, esim. "Nosto" tai "Talletus")
-// Summa (number)
-// Saldo ennen tapahtumaa (number)
+function hankiAika() {
+    var paivamaara = new Date();
+    paivaysIlmanUCT = paivamaara.getDate() +"."+ paivamaara.getMonth()   +"."+ paivamaara.getFullYear() +" "+ paivamaara.getHours() +"."+ paivamaara.getMinutes() + "." +paivamaara.getSeconds();
+    
+    return paivaysIlmanUCT;
+}
